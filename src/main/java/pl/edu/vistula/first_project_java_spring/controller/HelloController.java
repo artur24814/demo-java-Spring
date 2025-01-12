4f35db03369c9ed243f46e991bd6b2f5931393a2
Artur Yurchenko <artur24814@gmail.com>
2025-01-12 10:22:20 +0100

    Greeting endpoint has been added

--- src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -1,6 +1,8 @@
 package pl.edu.vistula.first_project_java_spring.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,14 @@ public class HelloController {
     @GetMapping(value = "/")
     public String hello(){
         return "Hello Vistula in my first controller";
+    }
+
+    @GetMapping(value = "/greeting")
+    public String greeting(
+        @RequestParam(name="name", required=false, defaultValue="World") String name,
+        Model model
+    ) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
